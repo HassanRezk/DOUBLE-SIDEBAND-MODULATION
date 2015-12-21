b188b1d7d205a15a08a621dc093b010c8b4a30b5
sherine sameh <sherine-sameh@hotmail.com>
2015-12-21 21:14:27 +0200

    undo

--- p2UseIdealFilter.m
+++ p2UseIdealFilter.m
@@ -1,10 +1,7 @@
 clc;
 %filtering the signal ==> 'removing all frequencies > 4KHZ'
-%filter = ones(1, length(Signalspectrum)).*(n1 >= -4000 & n1 <= 4000);
-f1 = ones(4000,2);
-f2 = zeros(length(Signalspectrum)-4000,2);
-filter =[f1 ; f2];
-filteredSpectrum = Signalspectrum.*filter;
+filter = ones(1, length(Signalspectrum)).*(n1 >= -4000 & n1 <= 4000);
+filteredSpectrum = Signalspectrum.*filter';
 figure;
 stem(n1, abs(filteredSpectrum));%plot the filtered signal
 legend('Spectrum of the filtered Signal');
