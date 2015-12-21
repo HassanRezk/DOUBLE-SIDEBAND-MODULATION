950c27d516cef40b3a9af2ab19a61b644c5e908c
sherine sameh <sherine-sameh@hotmail.com>
2015-12-21 02:03:57 +0200

    p1_modified

--- p1LoadSound.m
+++ p1LoadSound.m
@@ -1,12 +1,12 @@
 clc;clear all;
 [audioSignal, fs] = wavread('piano2.wav');%Read the audio file
-sound(audioSignal);%listen to the audio signal
+sound(audioSignal,fs);%listen to the audio signal
 figure;
 plot(audioSignal);%plot the waveform as x(t)
 legend('Wave form of the recorded Signal');
 xlabel('Time(seconds)');
 ylabel('Ampliture(volt)');
-Signalspectrum = fftshift(fft(audioSignal));%get the spectrum
+Signalspectrum =1/fs*(fftshift(fft(audioSignal)));%get the spectrum
 figure;
 n1 = linspace(-fs/2, fs/2, length(Signalspectrum));
 stem(n1, abs(Signalspectrum));%plot the spectrum
