fd62c41d1a6477c2e77e0fb9cdd08fd254755044
Hassan Rezk <hassan.rms93@gmail.com>
2015-12-25 22:22:54 +0200

    Add resample and modify modulation equation.

--- p5_Modulation.m
+++ p5_Modulation.m
@@ -3,34 +3,28 @@ clc;
 %audio signal sampling frequency must be changed
 %Dc bias = 2*max of the message
 %y = modulate(x,fc,fs,'amdsb-tc',opt)/
-Fc = 1*10^6;
+Fc = 100000;
 Fs = 3*Fc;
 
-F = linspace(-Fs, Fs, length(WaveFormOfFilteredSignal));
-carrier = cos(2*pi*Fc*F);
-carrier = carrier';
-modulated = carrier.*WaveFormOfFilteredSignal;
-M_SC_F = 1/Fs*(fftshift(fft(modulated)));
-stem(F, abs(M_SC_F))
+WaveFormOfFilteredSignal = resample(WaveFormOfFilteredSignal, Fs, fs);
 
+t = (0:1/Fs:((size(WaveFormOfFilteredSignal, 1)-1)/Fs))';
+t = t(:, ones(1, size(WaveFormOfFilteredSignal, 2)));
+
+vc = 2*max(real(WaveFormOfFilteredSignal));
+
+carrier = cos(2*pi*Fc*t);
+M_SC = carrier.*WaveFormOfFilteredSignal;
+M_SC = 1/Fs*(fftshift(fft(M_SC)));
+F = linspace(-Fs/2, Fs/2, length(WaveFormOfFilteredSignal));
+stem(F, abs(M_SC))
 
 figure;
-F = linspace(-Fs, Fs, length(WaveFormOfFilteredSignal));
-carrier = cos(2*pi*Fc*F);
-carrier = carrier';
-modulated = carrier.*WaveFormOfFilteredSignal;
-M_SC_F = 1/Fs*(fftshift(fft(modulated)));
-vc = 2*max(WaveFormOfFilteredSignal);
-Fcl = linspace(-Fs/2, Fs/2, length(WaveFormOfFilteredSignal));
-newcarrier = vc*cos(2*pi*Fc*Fcl);
-fourier_carrier = 1/Fs*(fftshift(fft(newcarrier)));
-M_TC_F = M_SC_F + fourier_carrier';
-stem(Fcl, abs(M_TC_F));
+M_TC = (vc + WaveFormOfFilteredSignal).*carrier;
+M_TC = 1/Fs*(fftshift(fft(M_TC)));
+stem(F, abs(M_TC))
 
-
-%stem(F, abs(M_SC_F))
 %plot(F, modulated);
-
 
 %M_SC_T = modulate(WaveFormOfFilteredSignal,Fc,Fs,'amdsb-sc');
 %M_SC_F = 1/Fs*(fftshift(fft(M_SC_T)));
--- p6_Envelope.m
+++ p6_Envelope.m
@@ -1,12 +1,12 @@
 
-SC_time = Fs*ifft(M_SC_F);
+SC_time = Fs*ifft(M_SC);
 plot(abs(SC_time))
 SC_received = abs(hilbert(SC_time));
 sound(abs(SC_received))
 figure
 plot(abs(SC_received))
 
-TC_time = Fs*ifft(M_TC_F);
+TC_time = Fs*ifft(M_TC);
 figure
 plot(abs(TC_time))
 TC_received = abs(hilbert(TC_time));
