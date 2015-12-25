29133110322866f3bd3b24d64d56b07139166269
Hassan Rezk <hassan.rms93@gmail.com>
2015-12-25 17:41:40 +0200

    Add Manual Modulation (DSBSC, DSBTC).

--- p12_power_efficiency.m
+++ p12_power_efficiency.m
@@ -1,8 +1,8 @@
 clc;
 %12. Calculate the power efficiency for both types of modulation. Comment
-for_suppressed_carrier = 1;
+for_suppressed_carrier = 1;%ps/ps
 %for transmitted carrier
-ps = sum(abs(M_SC_F).^2);
-pc = vc.^2/2;
+ps = real(sum(abs(M_SC_F).^2));
+pc = real(vc.^2/2);
 
-n = ps/(pc + ps)
+n = (ps/(pc + ps)) * 100
--- p5_Modulation.m
+++ p5_Modulation.m
@@ -6,15 +6,45 @@ clc;
 Fc = 1*10^6;
 Fs = 3*Fc;
 
-M_SC_T = modulate(WaveFormOfFilteredSignal,Fc,Fs,'amdsb-sc');
-M_SC_F = 1/Fs*(fftshift(fft(M_SC_T)));
-F = linspace(-Fs , Fs , length(M_SC_F));
-stem(F,abs(M_SC_F))
+F = linspace(-Fs, Fs, length(WaveFormOfFilteredSignal));
+carrier = cos(2*pi*Fc*F);
+carrier = carrier';
+modulated = carrier.*WaveFormOfFilteredSignal;
+M_SC_F = 1/Fs*(fftshift(fft(modulated)));
+stem(F, abs(M_SC_F))
 
 
-figure
-mx = max(audioSignal);
-vc = 2*mx;
-M_TC_T = ammod(abs(WaveFormOfFilteredSignal), Fc, Fs, 0, vc);
-M_TC_F = 1/Fs*(fftshift(fft(M_TC_T)));
-stem(F,abs(M_TC_F))
+figure;
+F = linspace(-Fs, Fs, length(WaveFormOfFilteredSignal));
+carrier = cos(2*pi*Fc*F);
+carrier = carrier';
+modulated = carrier.*WaveFormOfFilteredSignal;
+M_SC_F = 1/Fs*(fftshift(fft(modulated)));
+vc = 2*max(WaveFormOfFilteredSignal);
+Fcl = linspace(-Fs/2, Fs/2, length(WaveFormOfFilteredSignal));
+newcarrier = vc*cos(2*pi*Fc*Fcl);
+fourier_carrier = 1/Fs*(fftshift(fft(newcarrier)));
+M_TC_F = M_SC_F + fourier_carrier';
+stem(Fcl, abs(M_TC_F));
+
+
+%stem(F, abs(M_SC_F))
+%plot(F, modulated);
+
+
+%M_SC_T = modulate(WaveFormOfFilteredSignal,Fc,Fs,'amdsb-sc');
+%M_SC_F = 1/Fs*(fftshift(fft(M_SC_T)));
+%F = linspace(-Fs/2 , Fs/2 , length(M_SC_F));
+%stem(F,abs(M_SC_F))
+
+
+%figure
+%mx = max(audioSignal);
+%vc = 2*mx;
+%carrier = vc*cos(2*pi*Fc.*F);
+%fourier_carrier = 1/Fs*(fftshift(fft(carrier)));
+%stem(F, abs(fourier_carrier))
+
+%M_TC_T = ammod(abs(WaveFormOfFilteredSignal), Fc, Fs, 0, vc);
+%M_TC_F = 1/Fs*(fftshift(fft(M_TC_T)));
+%stem(F,abs(M_TC_F))
